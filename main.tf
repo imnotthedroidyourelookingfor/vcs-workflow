8c062617de9b4f19ae9b83f7c343eaf16e090f26
david_meister@csx.com <David_Meister@csx.com>
2024-11-06 10:57:07 -0500

    initial commit

---
+++ main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "2.3.0"
+    }
+  }
+  # cloud {
+  #   organization = "droid-demo-org"
+  #   workspaces {
+  #     name = "droid-cli-workspace"
+  #   }
+  # }
+}
+
+provider "local" {
+  # Configuration options
+}
+
+variable "product" {
+  type        = string
+  description = "A HashiCorp product name"
+  default     = "Terraform"
+}
+
+resource "local_file" "tf_file" {
+  content  = "\n This is a CSX ${var.product} workshop \n\n"
+  filename = "${path.module}/terraform.txt"
+}
+
+output "text" {
+  value = local_file.tf_file.content
+}
